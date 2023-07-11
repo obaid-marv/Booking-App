bda92c08b29160e42acc47008bd9bc1abd74733c
obaid-marv <0rehmanubaid@gmail.com>
2023-07-12 00:24:44 +0500

    commit

--- client/booking-application/src/components/header/Header.jsx
+++ client/booking-application/src/components/header/Header.jsx
@@ -1,7 +1,32 @@
 import "./header.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBed, faPlane, faCar, faMountainSun, faTaxi, faCalendarDays, faPerson} from "@fortawesome/free-solid-svg-icons";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { format } from "date-fns";
+
 const Header = ()=>{
+    
+    const [openDate, setOpenDate] = useState(false);
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
+    
+    const [openOptions, seOpenOptions] = useState(false);
+    const [options, setOptions] = useState(
+        {
+            adult:1,
+            children:0,
+            room:1
+        }
+    )
+
     return(
         <div className="header">
             <div className="headerContainer">
@@ -38,11 +63,18 @@ const Header = ()=>{
                     </div>
                     <div className="searchItem">
                         <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                        <span className="searchText">Day to Day</span>
+                        <span onClick={()=>{setOpenDate(!openDate)}} className="searchText">{`${format(date[0].startDate,"dd/MM/yy")} to ${format(date[0].endDate,"dd/MM/yy")}`}</span>
+                    { openDate && <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className="date"
+                    />}
                     </div>
                     <div className="searchItem">
                         <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-                        <span className="searchText">2 Adults 2 children 1 room</span>
+                    <span className="searchText">{`${options.adult} adult . ${options.children} children . ${options.room} room` }</span>
                     </div>
                     <div className="searchItem">
                         <button className="headerButton">Search</button>
