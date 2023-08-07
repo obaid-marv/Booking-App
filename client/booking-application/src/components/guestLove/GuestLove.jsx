31d95dd4de96f656c3a6f69452a6e6a30f2645cd
obaid-marv <0rehmanubaid@gmail.com>
2023-08-07 19:41:06 +0500

    commit

---
+++ client/booking-application/src/components/guestLove/GuestLove.jsx
@@ -0,0 +1,56 @@
+import './guestLove.css'
+
+const GuestLove = () => {
+  return (
+    <div className="gl">
+        <div className="glItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="glImg" />
+            <span className="glName">Aparthotel Stare Miasto</span>
+            <span className="glCity">OldTown,Poland</span>
+            <span className="glPrice">Sarting from $323</span>
+            <div className="glRating">
+                <button>7.9</button>
+                <span>Good</span>
+            </div>
+        </div>
+
+        <div className="glItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="glImg" />
+            <span className="glName">Aparthotel Stare Miasto</span>
+            <span className="glCity">OldTown,Poland</span>
+            <span className="glPrice">Sarting from $323</span>
+            <div className="glRating">
+                <button>7.9</button>
+                <span>Good</span>
+            </div>
+        </div>
+
+        <div className="glItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="glImg" />
+            <span className="glName">Aparthotel Stare Miasto</span>
+            <span className="glCity">OldTown,Poland</span>
+            <span className="glPrice">Sarting from $323</span>
+            <div className="glRating">
+                <button>7.9</button>
+                <span>Good</span>
+            </div>
+        </div>
+
+
+        <div className="glItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="glImg" />
+            <span className="glName">Aparthotel Stare Miasto</span>
+            <span className="glCity">OldTown,Poland</span>
+            <span className="glPrice">Sarting from $323</span>
+            <div className="glRating">
+                <button>7.9</button>
+                <span>Good</span>
+            </div>
+        </div>
+
+
+    </div>
+    )
+}
+
+export default GuestLove
--- client/booking-application/src/pages/home/Home.jsx
+++ client/booking-application/src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Featured from "../../components/featured/Featured";
+import GuestLove from "../../components/guestLove/GuestLove";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = ()=>{
@@ -10,6 +12,11 @@ const Home = ()=>{
         <Header/>
         <div className="homeContainer">
             <Featured/>
+            <h1 className="homeTitle">Browse by Property Type</h1>
+            <PropertyList/>
+            <h1 className="homeTitle">Homes guests love</h1>
+            <GuestLove/>
+
         </div>
     </div>
     )
